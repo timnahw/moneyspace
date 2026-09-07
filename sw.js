1690490f81f13b6f273cf9743974c9fefa840b59
timnahw <156186100+timnahw@users.noreply.github.com>
2026-09-08 00:14:09 +0200

    Publish Moneyspace

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'moneyspace-shell-v1';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    const copy = response.clone();
+    if (new URL(event.request.url).origin === self.location.origin) {
+      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+    }
+    return response;
+  })));
+});
